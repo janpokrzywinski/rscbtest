1d4284d7e535ba9283db1f86855e8002a40c50c4
janpokrzywinski <jan.pokrzywinski@rackspace.co.uk>
2015-11-24 18:17:12 +0000

    adding function to print headers, print uname and visual cleanup of code, adding brackets to variables

--- backuptest.sh
+++ backuptest.sh
@@ -3,14 +3,14 @@
 # Jan Pokrzywinski (Rackspace UK) 2015
 # Version: 1.4.2 (2015-07-01)
 
-# Check if output is directed to file or to terminali and set variables for colors (Used to strip colors for text output)
-
+# Check if script is executed as root, if not break
 if [ $(whoami) != "root" ]
 then
     echo "ERROR: This script needs to be run as root!"
     exit 1
 fi
 
+# Check if output is directed to file or to terminali and set variables for colors (Used to strip colors for text output)
 if [ -t 1 ]
 then
     ColourMag="\e[1;36m"
@@ -30,18 +30,18 @@ fi
 CurrentRegion=$(xenstore-read vm-data/provider_data/region 2>&1)
 
 # Set numbers of endpoints to resolve/ping based on CurrentRegion
-if [ $CurrentRegion == 'hkg' ] || [ $CurrentRegion == 'syd' ] || [ $CurrentRegion == 'iad' ]
+if [ ${CurrentRegion} == 'hkg' ] || [ ${CurrentRegion} == 'syd' ] || [ ${CurrentRegion} == 'iad' ]
 then
     EndpointNumber=4
-elif [ $CurrentRegion == 'dfw' ] || [ $CurrentRegion == 'ord' ]
+elif [ ${CurrentRegion} == 'dfw' ] || [ ${CurrentRegion} == 'ord' ]
 then
     EndpointNumber=3
-elif [ $CurrentRegion == 'lon' ]
+elif [ ${CurrentRegion} == 'lon' ]
 then
     EndpointNumber=5
 else
-    echo -e "$ColourRed ERROR : Cannot read the region from XenStore data!$NoColour\nIs this Rackspace Cloud Server?\nPlease check if xe-daemon is running and check if xe-linux-distribution service is set to start at boot.\n"
-    echo -e "$ColourRed Test results may be inconsistent due to this!$NoColour\n"
+    echo -e "${ColourRed}ERROR : Cannot read the region from XenStore data!${NoColour}\nIs this Rackspace Cloud Server?\nPlease check if xe-daemon is running and check if xe-linux-distribution service is set to start at boot.\n"
+    echo -e "${ColourRed}Test results may be inconsistent due to this!${NoColour}\n"
     EndpointNumber=3
 fi
 
@@ -55,7 +55,7 @@ declare -A Region
     Region[ord]="ord1"
 
 # Setting up endpoint hostnames for ping
-if [ $CurrentRegion == 'lon' ]
+if [ ${CurrentRegion} == 'lon' ]
 then
 Endpoint=(
     "api.drivesrvr.com"
@@ -64,7 +64,7 @@ Endpoint=(
     "snet-storage101.${Region[$CurrentRegion]}.clouddrive.com"
     "rse.drivesrvr.co.uk"
     "api.drivesrvr.co.uk"
-    "rse.$CurrentRegion.drivesrvr.com"
+    "rse.${CurrentRegion}.drivesrvr.com"
     )
 else
 Endpoint=(
@@ -72,95 +72,107 @@ Endpoint=(
     "rse.drivesrvr.com"
     "storage101.${Region[$CurrentRegion]}.clouddrive.com"
     "snet-storage101.${Region[$CurrentRegion]}.clouddrive.com"
-    "rse.$CurrentRegion.drivesrvr.com"
+    "rse.${CurrentRegion}.drivesrvr.com"
     )
 fi
 
+print_header () {
+    echo -e "\n${ColourMag}>======== $1:${NoColour}"
+}
+
+
 # Resolve all access points for all regions
-echo -e "\n$ColourMag>======== Test DNS resolution:$NoColour"
-for ResNumber in $(seq 0 $EndpointNumber)
+#echo -e "\n${ColourMag}>======== Test DNS resolution:${NoColour}"
+print_header "Test DNS resolution"
+for ResNumber in $(seq 0 ${EndpointNumber})
 do
     host ${Endpoint[ResNumber]}
 done    
 
 # Run single ping request to each of the access points
-echo -e "\n$ColourMag>======== Test ping:$NoColour"
+echo -e "\n${ColourMag}>======== Test ping:${NoColour}"
 for PingNumber in $(seq 0 $EndpointNumber)
 do
     if ping -q -W3 -c1 ${Endpoint[PingNumber]} &> /dev/null
     then
-        echo -e "Ping to$ColourBlue ${Endpoint[PingNumber]} $NoColour:$ColourGreen Success $NoColour"
+        echo -e "Ping to${ColourBlue} ${Endpoint[PingNumber]} ${NoColour}:${ColourGreen} Success ${NoColour}"
     else
-        echo -e "Ping to$ColourBlue ${Endpoint[PingNumber]} $NoColour:$ColourRed Error $NoColour"
+        echo -e "Ping to${ColourBlue} ${Endpoint[PingNumber]} ${NoColour}:${ColourRed} Error ${NoColour}"
     fi
 done
 
 # Showing network interface configuration and routes routes
-echo -e "\n$ColourMag>======== Network settings:$NoColour"
+print_header "Network settings"
     route -n
     echo
     ifconfig eth0 | head -3
     echo
     ifconfig eth1 | head -3
-    echo -e "\n$ColourBlue> DNS settings (contents of resolv.conf):$NoColour"
+    echo -e "\n${ColourBlue}> DNS settings (contents of resolv.conf):${NoColour}"
     cat /etc/resolv.conf
 
 # Check Backup API health status:
-echo -e "\n$ColourMag>======== API Nodes status:$NoColour"
+print_header "API Nodes status"
 echo "Status of https://rse.drivesrvr.com/health :"
 curl -s https://rse.drivesrvr.com/health
-echo "Status of https://$CurrentRegion.backup.api.rackspacecloud.com/v1.0/help/apihealth :"
-curl -s https://$CurrentRegion.backup.api.rackspacecloud.com/v1.0/help/apihealth
-echo -e "\nStatus of https://$CurrentRegion.backup.api.rackspacecloud.com/v1.0/help/health :"
-curl -s https://$CurrentRegion.backup.api.rackspacecloud.com/v1.0/help/health
+echo "Status of https://${CurrentRegion}.backup.api.rackspacecloud.com/v1.0/help/apihealth :"
+curl -s https://${CurrentRegion}.backup.api.rackspacecloud.com/v1.0/help/apihealth
+echo -e "\nStatus of https://${CurrentRegion}.backup.api.rackspacecloud.com/v1.0/help/health :"
+curl -s https://${CurrentRegion}.backup.api.rackspacecloud.com/v1.0/help/health
 echo
 
 # Show contents from bootstrap.json (config file)
-echo -e "\n$ColourMag>======== Bootstrap contents:$NoColour"
+print_header "Bootstrap contents"
     cat /etc/driveclient/bootstrap.json
 
 # Listing processes and checking if backup agent is present
-echo -e "\n$ColourMag>======== Processes running:$NoColour"
+print_header "Processes running"
     ps aux | grep '[d]riveclient\|[c]loudbackup-updater'
     if [ "$(pidof driveclient)" ] 
     then
         # process running
-        echo -e "$ColourGreen> driveclient process present $NoColour"
+        echo -e "${ColourGreen}> driveclient process present ${NoColour}"
     else
-        echo -e "$ColourRed> driveclient service is not running! $NoColour\nIf the agent is installed and configured correctly run this:\nservice driveclient start\n"
+        echo -e "${ColourRed}> driveclient service is not running! ${NoColour}\nIf the agent is installed and configured correctly run this:\nservice driveclient start\n"
     fi
 
 # Location of the binary
-echo -e "\n$ColourMag>======== Location of binaries (whereis):$NoColour"
+print_header "Location of binaries (whereis)"
     whereis driveclient
     whereis cloudbackup-updater
 
 # Check version of driveclient
-echo -e "\n$ColourMag>======== Driveclient version:$NoColour"
+print_header "Driveclient version"
     driveclient --version
 
+# Show kernel information
+print_header "Kernel information (uname)"
+    uname -a
+
 # Show contents of cache directory and check if lock file exists
-echo -e "\n$ColourMag>======== Cache directory conents (/var/cache/driveclient/):$NoColour"
+print_header "Cache directory contents (/var/cache/driveclient/)"
     ls -hla /var/cache/driveclient/
 
 LockFile=/var/cache/driveclient/backup-running.lock
-    if [ -f $LockFile ];
+    if [ -f ${LockFile} ];
     then
-        echo -e "\n$ColourRed!!! Lock File present! ($LockFile)\e$NoColour\nIf backups are stuck in queued state or showing as skipped for last few attempts follow these steps:\n1) Stop backup task in control panel\n2) Stop driveclient service\n3) Delete lock file\n4) Start driveclient service"
+        echo -e "\n${ColourRed}!!! Lock File present! (${LockFile})\e${NoColour}\nIf backups are stuck in queued state or showing as skipped for last few attempts follow these steps:\n1) Stop backup task in control panel\n2) Stop driveclient service\n3) Delete lock file\n4) Start driveclient service"
     fi
 
+LogFile=/var/log/driveclient.log
 # Last 10 entries of log file
-echo -e "\n$ColourMag>======== Last 10 log entries:$NoColour"
-    tail /var/log/driveclient.log
+print_header "Last 10 entries from the log file (${LogFile})"
+    tail ${LogFile}
 
 # Checking just for log entries containing "err"
-echo -e "\n$ColourMag>======== Last 5 Errors in log:$NoColour"
-    grep -i err /var/log/driveclient.log | tail -5
+print_header "Last 5 Errors in log file (${LogFile})"
+    grep -i err ${LogFile} | tail -5
 
 # Show disk space and inodes
-echo -e "\n$ColourMag>======== Disk space and inodes:$NoColour"
+print_header "Disk space left and inodes"
     df -h
     echo
     df -i
+
 # Clear echo to give clean closing
 echo
